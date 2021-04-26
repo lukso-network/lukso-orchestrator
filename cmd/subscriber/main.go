f39ba61ee43bd91809acda58d194d554e71cc5a9
blazejkrzak <blazej@m8b.pl>
2021-04-26 17:43:26 +0200

    Added string cast to info

--- cmd/subscriber/main.go
+++ cmd/subscriber/main.go
@@ -12,7 +12,7 @@ import (
 
 // wsRequest attempts to open a WebSocket connection to the given URL.
 func wsRequest(url, browserOrigin string) error {
-	log.Info("checking WebSocket on %s (origin %q)", url, browserOrigin)
+	log.Info("checking WebSocket on %s (origin %q)", string(url), browserOrigin)
 
 	headers := make(http.Header)
 	if browserOrigin != "" {
