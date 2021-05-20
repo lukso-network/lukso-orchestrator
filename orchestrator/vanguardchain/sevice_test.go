379b36e0a8eb52555cb8e3a7401d9abf49e8ae5a
blazejkrzak <blazej@m8b.pl>
2021-05-20 14:13:11 +0200

    Feature: 32 consensus info via grpc - commit tests

--- orchestrator/vanguardchain/sevice_test.go
+++ orchestrator/vanguardchain/sevice_test.go
@@ -118,7 +118,7 @@ func Test_VanguardSvc_RetryToConnServer(t *testing.T) {
 
 	time.Sleep(10 * reConPeriod)
 	assert.LogsContainNTimes(t, hook, "Could not connect to vanguard endpoint", 10)
-	time.Sleep(reConPeriod * 2)
+	time.Sleep(time.Second)
 	shouldPass = true
 
 	time.Sleep(reConPeriod)
