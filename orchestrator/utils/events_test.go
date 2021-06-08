76b6d301b9b9587b5f6490ba637af816efe3a92d
blazejkrzak <blazej@m8b.pl>
2021-06-08 11:08:05 +0200

    Feature: 12 - WIP: Debounce function should work. It has all the tests that are needed

--- orchestrator/utils/events_test.go
+++ orchestrator/utils/events_test.go
@@ -8,34 +8,53 @@ import (
 )
 
 func Test_Debounce(t *testing.T) {
-	capacity := 20
-	eventChannel := make(chan interface{}, capacity)
-	ctx := context.Background()
-	interval := time.Millisecond * 10
-	triggeredChannel := make(chan bool)
+	debounceFunc := func(
+		capacity int,
+		interval time.Duration,
+		checkAfter time.Duration,
+		expectedHandlerCalls int,
+	) {
+		eventChannel := make(chan interface{}, capacity)
+		ctx := context.Background()
+		triggeredChannel := make(chan bool)
 
-	handler := func(triggeredTimes interface{}) {
-		triggeredChannel <- true
-	}
-
-	dummyEvent := struct {
-		Something int
-	}{}
-
-	for index := 0; index < capacity; index++ {
-		eventChannel <- dummyEvent
-	}
-
-	triggeredTimes := 0
-
-	go Debounce(ctx, interval, eventChannel, handler)
-	go func() {
-		for {
-			<-triggeredChannel
-			triggeredTimes++
+		handler := func(triggeredTimes interface{}) {
+			triggeredChannel <- true
 		}
-	}()
 
-	time.Sleep(interval*time.Duration(capacity) + time.Millisecond*2)
-	require.Equal(t, 1, triggeredTimes)
+		dummyEvent := struct {
+			Something int
+		}{}
+
+		for index := 0; index < capacity; index++ {
+			eventChannel <- dummyEvent
+		}
+
+		triggeredTimes := 0
+
+		go Debounce(ctx, interval, eventChannel, handler)
+		go func() {
+			for {
+				<-triggeredChannel
+				triggeredTimes++
+			}
+		}()
+
+		time.Sleep(checkAfter)
+		require.Equal(t, expectedHandlerCalls, triggeredTimes)
+	}
+
+	t.Run("should be invoked 1 time", func(t *testing.T) {
+		capacity := 20
+		interval := time.Millisecond * 10
+		checkAfter := interval*time.Duration(capacity) + time.Millisecond*2
+		debounceFunc(capacity, interval, checkAfter, 1)
+	})
+
+	t.Run("should not be invoked", func(t *testing.T) {
+		capacity := 500000
+		interval := time.Millisecond * 10
+		checkAfter := time.Millisecond * 15
+		debounceFunc(capacity, interval, checkAfter, 0)
+	})
 }
