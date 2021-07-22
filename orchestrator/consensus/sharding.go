94deadba3a79539e2a0dab7e18fc1b66b2574bab
muhaimin <muhaimin@cefalo.com>
2021-07-22 16:51:34 +0600

    sharding comparison implemented without test

--- orchestrator/consensus/sharding.go
+++ orchestrator/consensus/sharding.go
@@ -1,27 +1,83 @@
 package consensus
 
 import (
-	"github.com/lukso-network/lukso-orchestrator/shared/types"
 	"reflect"
+
+	eth1Types "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/lukso-network/lukso-orchestrator/shared/types"
+	eth2Types "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 )
 
-func CompareShardingInfo(ob1, ob2 *types.HeaderHash) bool {
-	if ob1 == nil && ob2 == nil {
+func CompareShardingInfo(pandoraHeaderData *eth1Types.Header, vanguardShardInfo *eth2Types.PandoraShard) bool {
+	if pandoraHeaderData == nil && vanguardShardInfo == nil {
 		// in existing code this will happen. as some part may have no sharding info for testing.
 		return true
 	}
-	// TODO: IT WILL OPEN AFTER RESOLVING HASHING PROBLEM IN VANGUARD
-	if !reflect.DeepEqual(ob1.PandoraShardHash, ob2.PandoraShardHash) {
-		log.WithField("object1 hash", ob1.PandoraShardHash).
-			WithField("object2 hash", ob2.PandoraShardHash).
-			Error("hash mismatched")
+
+	if vanguardShardInfo.BlockNumber != pandoraHeaderData.Number.Uint64() {
+		log.WithField("pandora data block number", pandoraHeaderData.Number.Uint64()).
+			WithField("vanguard block number", vanguardShardInfo.BlockNumber).
+			Error("block number mismatched")
 		return false
 	}
-	if !reflect.DeepEqual(ob1.Signature, ob2.Signature) {
-		log.WithField("object1 Signature", ob1.Signature).
-			WithField("object2 Signature", ob2.Signature).
-			Error("Signature mismatched")
+
+	// match header hash
+	if !reflect.DeepEqual(pandoraHeaderData.Hash().Bytes(), vanguardShardInfo.GetHash()) {
+		log.WithField("pandora header hash", pandoraHeaderData.Hash().Bytes()).
+			WithField("vanguard header hash", vanguardShardInfo.GetHash()).
+			Error("header hash mismatched")
 		return false
 	}
+
+	// match parent hash
+	if !reflect.DeepEqual(pandoraHeaderData.ParentHash.Bytes(), vanguardShardInfo.GetParentHash()) {
+		log.WithField("pandora data parent hash", pandoraHeaderData.ParentHash.Bytes()).
+			WithField("vanguard parent hash", vanguardShardInfo.ParentHash).
+			Error("parent hash mismatched")
+		return false
+	}
+
+	// match state root hash
+	if !reflect.DeepEqual(pandoraHeaderData.Root.Bytes(), vanguardShardInfo.GetStateRoot()) {
+		log.WithField("pandora data root hash", pandoraHeaderData.Root.Bytes()).
+			WithField("vanguard state root hash", vanguardShardInfo.StateRoot).
+			Error("state root hash mismatched")
+		return false
+	}
+
+	// match TxHash
+	if !reflect.DeepEqual(pandoraHeaderData.TxHash.Bytes(), vanguardShardInfo.GetTxHash()) {
+		log.WithField("pandora data tx hash", pandoraHeaderData.TxHash.Bytes()).
+			WithField("vanguard tx hash", vanguardShardInfo.TxHash).
+			Error("tx hash mismatched")
+		return false
+	}
+
+	// match receiptHash
+	if !reflect.DeepEqual(pandoraHeaderData.ReceiptHash.Bytes(), vanguardShardInfo.GetReceiptHash()) {
+		log.WithField("pandora data receipt hash", pandoraHeaderData.ReceiptHash.Bytes()).
+			WithField("vanguard receipt hash", vanguardShardInfo.ReceiptHash).
+			Error("receipt hash mismatched")
+		return false
+	}
+
+	// retrieve extra data
+	pandoraExtraDataWithSig := new(types.PanExtraDataWithBLSSig)
+	err := rlp.DecodeBytes(pandoraHeaderData.Extra, pandoraExtraDataWithSig)
+	if nil != err {
+		log.WithField("error", err).
+			Error("error converting extra data to extraDataWithSig")
+		return false
+	}
+
+	// match signature
+	if !reflect.DeepEqual(pandoraExtraDataWithSig.BlsSignatureBytes.Bytes(), vanguardShardInfo.GetSignature()) {
+		log.WithField("pandora data signature", pandoraExtraDataWithSig.BlsSignatureBytes.Bytes()).
+			WithField("vanguard signature", vanguardShardInfo.GetSignature()).
+			Error("signature mismatched")
+		return false
+	}
+
 	return true
 }
