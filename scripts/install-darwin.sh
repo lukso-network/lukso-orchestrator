1dba52e0047d4e7d10ecc42fa94d81acc3d4794f
mxmar <macht4tech@gmail.com>
2021-07-28 19:46:12 +0200

    - Added darwin support;
    Signed-off-by: mxmar <macht4tech@gmail.com>

---
+++ scripts/install-darwin.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+RELEASE_TAG=v0.0.28-beta;
+sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/lukso-darwin -O /usr/local/bin/lukso &&
+sudo wget https://raw.githubusercontent.com/lukso-network/lukso-orchestrator/feature/47/installation-script/scripts/lukso-cli -O /usr/local/bin/lukso-cli &&
+sudo chmod +x /usr/local/bin/lukso &&
+sudo chmod +x /usr/local/bin/lukso-cli &&
+echo "Type lukso-cli to start the node";
