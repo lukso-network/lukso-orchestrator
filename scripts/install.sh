0b4a40e0bd34c06cf7dd9224cf4b16ee3c637e45
Patryk Krakos <patryk.krakos@m8b.pl>
2021-07-28 12:01:14 +0200

    changed version

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RELEASE_TAG=v0.0.28-alpha;
+RELEASE_TAG=v0.0.28-beta;
 sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/lukso -O /usr/local/bin/lukso &&
 sudo wget https://raw.githubusercontent.com/lukso-network/lukso-orchestrator/feature/47/installation-script/scripts/lukso-cli -O /usr/local/bin/lukso-cli &&
 sudo chmod +x /usr/local/bin/lukso &&
