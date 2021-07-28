fa009a04bf86facb9f73770b0d3f7d10cb92d01b
Patryk Krakos <patryk.krakos@m8b.pl>
2021-07-28 11:40:20 +0200

    Changed binary name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 RELEASE_TAG=v0.0.28-alpha;
-sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/celebrimbor -O /usr/local/bin/celebrimbor
-sudo chmod +x /usr/local/bin/celebrimbor
-echo ;
+sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/celebrimbor -O /usr/local/bin/celebrimbor &&
+sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/lukso-cli -O /usr/local/bin/lukso-cli &&
+sudo chmod +x /usr/local/bin/celebrimbor &&
+sudo chmod +x /usr/local/bin/lukso-cli &&
+echo "Type lukso-cli to start the node";
