170f5a315051f63c1db60eef5b0eba801287eb5b
Patryk Krakos <patryk.krakos@m8b.pl>
2021-07-28 11:52:30 +0200

    Bootnodes flag

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 RELEASE_TAG=v0.0.28-alpha;
-sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/celebrimbor -O /usr/local/bin/celebrimbor &&
-sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/lukso-cli -O /usr/local/bin/lukso-cli &&
-sudo chmod +x /usr/local/bin/celebrimbor &&
+sudo wget https://github.com/lukso-network/lukso-orchestrator/releases/download/$RELEASE_TAG/lukso -O /usr/local/bin/lukso &&
+sudo wget https://raw.githubusercontent.com/lukso-network/lukso-orchestrator/feature/47/installation-script/scripts/lukso-cli -O /usr/local/bin/lukso-cli &&
+sudo chmod +x /usr/local/bin/lukso &&
 sudo chmod +x /usr/local/bin/lukso-cli &&
 echo "Type lukso-cli to start the node";
