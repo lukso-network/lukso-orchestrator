8cb31acbb07e25c894cc1ab9fff237d921a11e42
Patryk Krakos <patryk.krakos@m8b.pl>
2021-08-31 13:05:57 +0200

    Imported files from: https://github.com/lukso-network/network-testnet-validator-setup/tree/feature/l15-setup

---
+++ scripts/install-unix.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+NETWORK="l15"
+REPOSITORY="http://localhost:8000/l15-cdn";
+PLATFORM="unknown";
+
+if [ "$OSTYPE" = "linux-gnu" ]; then
+  PLATFORM="linux";
+elif [ "$OSTYPE" = "darwin" ]; then
+  PLATFORM="darwin"
+elif [ "$OSTYPE" = "cygwin" ]; then
+  PLATFORM="linux"
+elif [ "$OSTYPE" = "msys" ]; then
+  PLATFORM="windows"
+elif [ "$OSTYPE" = "win32" ]; then
+  PLATFORM="windows"
+elif [ "$OSTYPE" = "freebsd" ]; then
+  PLATFORM="linux"
+fi
+
+if [ "$PLATFORM" = "unknown" ]; then
+  echo "Platform not supported.";
+  exit;
+fi
+
+sudo mkdir \
+/opt/lukso \
+/opt/lukso/tmp \
+/opt/lukso/networks \
+/opt/lukso/networks/"$NETWORK" \
+/opt/lukso/networks/"$NETWORK"/bin \
+/opt/lukso/networks/"$NETWORK"/config;
+
+
+if [ "$PLATFORM" = "linux" ]; then
+  sudo wget -O /opt/lukso/tmp/bin.zip "$REPOSITORY"/linux-binaries.zip;
+fi
+
+if [ "$PLATFORM" = "darwin" ]; then
+  sudo wget -O /opt/lukso/tmp/bin.zip "$REPOSITORY"/darwin-binaries.zip;
+fi
+
+sudo wget -O /opt/lukso/tmp/config.zip "$REPOSITORY"/config.zip;
+
+sudo unzip /opt/lukso/tmp/config.zip -d /opt/lukso/networks/"$NETWORK"/config;
+sudo unzip /opt/lukso/tmp/bin.zip -d /opt/lukso/networks/"$NETWORK"/bin;
+
+sudo chmod -R +x /opt/lukso/networks/"$NETWORK"/bin;
+
+sudo ln -s /opt/lukso/networks/"$NETWORK"/bin/lukso-cli /usr/local/bin/lukso-cli;
+
+sudo rm -rf /opt/lukso/tmp;
+
+echo "Ready! type lukso to start the node!";
