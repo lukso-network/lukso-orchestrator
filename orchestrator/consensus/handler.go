ba3884190e5228ee41bac04c945ddbd70a2435f6
Atif Anowar <atif@lukso.io>
2021-09-11 12:21:47 +0200

    Do not delete headers from pending cache in invalid status

--- orchestrator/consensus/handler.go
+++ orchestrator/consensus/handler.go
@@ -11,24 +11,22 @@ import (
 // processPandoraHeader
 func (s *Service) processPandoraHeader(headerInfo *types.PandoraHeaderInfo) error {
 	slot := headerInfo.Slot
-
+	s.pandoraPendingHeaderCache.Put(s.ctx, slot, headerInfo.Header)
 	vanShardInfo, _ := s.vanguardPendingShardingCache.Get(s.ctx, slot)
 	if vanShardInfo != nil {
 		return s.verifyShardingInfo(slot, vanShardInfo, headerInfo.Header)
 	}
-	s.pandoraPendingHeaderCache.Put(s.ctx, headerInfo.Slot, headerInfo.Header)
 	return nil
 }
 
 // processVanguardShardInfo
 func (s *Service) processVanguardShardInfo(vanShardInfo *types.VanguardShardInfo) error {
 	slot := vanShardInfo.Slot
-
+	s.vanguardPendingShardingCache.Put(s.ctx, slot, vanShardInfo)
 	headerInfo, _ := s.pandoraPendingHeaderCache.Get(s.ctx, slot)
 	if headerInfo != nil {
 		return s.verifyShardingInfo(slot, vanShardInfo, headerInfo)
 	}
-	s.vanguardPendingShardingCache.Put(s.ctx, vanShardInfo.Slot, vanShardInfo)
 	return nil
 }
 
@@ -43,12 +41,6 @@ func (s *Service) verifyShardingInfo(slot uint64, vanShardInfo *types.VanguardSh
 		PandoraHeaderHash: header.Hash(),
 		VanguardBlockHash: common.BytesToHash(vanShardInfo.BlockHash[:]),
 	}
-	//removing previous cached slots which dont verified yet. By convention, they are skipped
-	defer func() {
-		s.pandoraPendingHeaderCache.Remove(s.ctx, slot)
-		s.vanguardPendingShardingCache.Remove(s.ctx, slot)
-	}()
-
 	if status {
 		// store verified slot info into verified slot info bucket
 		if err := s.verifiedSlotInfoDB.SaveVerifiedSlotInfo(slot, slotInfo); err != nil {
@@ -57,15 +49,16 @@ func (s *Service) verifyShardingInfo(slot uint64, vanShardInfo *types.VanguardSh
 				"Failed to store verified slot info")
 			return err
 		}
-
 		if err := s.verifiedSlotInfoDB.SaveLatestVerifiedSlot(s.ctx); err != nil {
 			log.WithError(err).Error("Failed to store latest verified slot")
 		}
-
 		if err := s.verifiedSlotInfoDB.SaveLatestVerifiedHeaderHash(); err != nil {
 			log.WithError(err).Error("Failed to store latest verified slot")
 		}
 		slotInfoWithStatus.Status = types.Verified
+		//removing previous cached slots which dont verified yet. By convention, they are skipped
+		s.pandoraPendingHeaderCache.Remove(s.ctx, slot)
+		s.vanguardPendingShardingCache.Remove(s.ctx, slot)
 		log.WithField("slot", slot).Info("Successfully verified sharding info")
 	} else {
 		// store invalid slot info into invalid slot info bucket
@@ -78,7 +71,6 @@ func (s *Service) verifyShardingInfo(slot uint64, vanShardInfo *types.VanguardSh
 		slotInfoWithStatus.Status = types.Invalid
 		log.WithField("slot", slot).Info("Invalid sharding info")
 	}
-
 	// sending verified slot info to rpc service
 	s.verifiedSlotInfoFeed.Send(slotInfoWithStatus)
 	return nil
