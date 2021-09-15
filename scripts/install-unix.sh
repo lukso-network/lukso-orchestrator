034c6d31b3eec4ed110a7cedfb8df51c5f385360
Patryk Krakos <patryk.krakos@m8b.pl>
2021-09-15 11:47:59 +0200

    Add flags for different networks

--- scripts/install-unix.sh
+++ scripts/install-unix.sh
@@ -3,19 +3,20 @@
 NETWORK="l15"
 REPOSITORY="https://storage.googleapis.com/l16-common/l15-cdn";
 PLATFORM="unknown";
+ARCHITECTURE=$(uname -m);
 
 ORCHESTRATOR_TAG="";
 PANDORA_TAG="";
 VANGUARD_TAG="";
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
-  PLATFORM="linux";
+  PLATFORM="Linux";
 elif [[ "$OSTYPE" = "darwin"* ]]; then
-  PLATFORM="darwin"
+  PLATFORM="Darwin"
 elif [ "$OSTYPE" = "cygwin" ]; then
-  PLATFORM="linux"
+  PLATFORM="Linux"
 elif [ "$OSTYPE" = "freebsd" ]; then
-  PLATFORM="linux"
+  PLATFORM="Linux"
 fi
 
 if [ "$PLATFORM" = "unknown" ]; then
@@ -35,8 +36,6 @@ download() {
   fi
 }
 
-
-
 sudo mkdir \
 /opt/lukso \
 /opt/lukso/tmp \
@@ -45,7 +44,8 @@ sudo mkdir \
 /opt/lukso/networks/"$NETWORK" \
 /opt/lukso/networks/"$NETWORK"/config;
 
-download https://raw.githubusercontent.com/lukso-network/lukso-orchestrator/feature/l15-setup/scripts/lukso /opt/lukso/lukso;
+download https://raw.githubusercontent.com/lukso-network/lukso-orchestrator/feature/l15-setup-script/scripts/lukso /opt/lukso/lukso;
+
 download "$REPOSITORY"/config.zip /opt/lukso/tmp/config.zip;
 
 sudo unzip /opt/lukso/tmp/config.zip -d /opt/lukso/networks/"$NETWORK"/config;
@@ -54,5 +54,11 @@ sudo chmod +x /opt/lukso/lukso;
 sudo ln -sfn /opt/lukso/lukso /usr/local/bin/lukso;
 
 sudo rm -rf /opt/lukso/tmp;
+
+sudo lukso bind-binaries \
+--orchestrator v0.1.0-beta.1 \
+--pandora v0.1.0-beta.1 \
+--vanguard v0.1.0-beta.1 \
+--validator v0.1.0-beta.1;
 
 echo "Ready! type lukso to start the node!";
