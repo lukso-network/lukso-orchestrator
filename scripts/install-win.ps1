e9b851dfdf01033ad6b192446a19f94aaeb539b7
Patryk Krakos <patryk.krakos@m8b.pl>
2021-09-15 15:17:18 +0200

    Changed Install dir for Windows

--- scripts/install-win.ps1
+++ scripts/install-win.ps1
@@ -1,6 +1,6 @@
 $Network = "l15";
 $Repository = "https://storage.googleapis.com/l16-common/l15-cdn";
-$InstallDir = "/home/pk/tmp/dupa";
+$InstallDir = $Env:APPDATA;
 
 $client = New-Object System.Net.WebClient
 
