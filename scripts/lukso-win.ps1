522557e995f53264059436399f315612654e2949
Patryk Krakos <patryk.krakos@m8b.pl>
2021-10-20 14:56:49 +0200

    Add downloading binaries for windows

--- scripts/lukso-win.ps1
+++ scripts/lukso-win.ps1
@@ -10,8 +10,8 @@ param (
     [String]$eth2stats = "",
     [String]$network = "l15",
     [String]$lukso_home = "$HOME\.lukso",
-    [String]$datadir = "$lukso_home\datadir",
-    [String]$logsdir = "$lukso_home\logs",
+    [String]$datadir = "$lukso_home\$network\datadir",
+    [String]$logsdir = "$lukso_home\$network\logs",
     [String]$keys_dir = "",
     [String]$keys_password_file = "",
     [String]$wallet_dir = "",
@@ -37,29 +37,90 @@ param (
     [Switch]$force
 )
 
+$platform = "Windows"
+$architecture = "x86_64"
+$InstallDir = $Env:APPDATA+"\LUKSO";
+
 $runDate = Get-Date -Format "yyyy-m-dd__HH-mm-ss"
 
-function start_orchestrator() {
+Function download ($url, $dst) {
+    echo $url
+    echo $dst
+    $client = New-Object System.Net.WebClient
+    $client.DownloadFile($url, $dst)
+}
 
-    New-Item -ItemType Directory -Force -Path $logsdir\orchestrator
-    echo -n $runDate | Out-File -FilePath "$logsdir\orchestrator\current.tmp"
+Function download_binary ($client, $tag) {
+
+    switch ($client) {
+        orchestrator {
+            $repo = "lukso-orchestrator"
+        }
+
+        pandora {
+            $repo = "pandora-execution-engine"
+        }
+
+        vanguard {
+            $repo = "vanguard-consensus-engine"
+        }
+
+        validator {
+            $repo = "vanguard-consensus-engine"
+        }
+    }
+
+    $TARGET = "$InstallDir\binaries\$CLIENT\$TAG"
+    New-Item -ItemType Directory -Force -Path $TARGET
+    download "https://github.com/lukso-network/$repo/releases/download/$tag/$client-$platform-$architecture.exe" "$TARGET\$CLIENT-$PLATFORM-$ARCHITECTURE"
+
+}
+
+Function download_network_config ($network) {
+    $CDN = "https://storage.googleapis.com/l15-cdn/networks/"+$network
+    $TARGET = $InstallDir+"\networks\"+$network+"\config"
+    New-Item -ItemType Directory -Force -Path $TARGET
+    download $CDN"\network-config.yaml?ignoreCache=1" $TARGET"\network-config.yaml"
+}
+
+Function bind_binary($client, $tag) {
+    if (!(Test-Path "$InstallDir/binaries/$client/$tag/$client-$platform-$architecture")) {
+        download_binary $client $tag
+    }
+    rm "$InstallDir\globalPath\$client"
+    cmd /c mklink "$InstallDir\globalPath\$client" "$InstallDir\binaries\$client\$tag\$client-$platform-$architecture"
+}
+
+Function bind_binaries() {
+    Write-Output
+}
+
+Function start_orchestrator() {
 
     if ( ! (Test-Path "$datadir\orchestrator") ) {
        New-Item -ItemType Directory -Force -Path $datadir\orchestrator
     }
 
-Start-Process -FilePath "lukso-orchestrator" -ArgumentList "--datadir=$datadir\orchestrator", `
-"--vanguard-grpc-endpoint=127.0.0.1:4000", `
-"--http", `
-"--http.addr=0.0.0.0",  `
-"--http.port=7877", `
-"--ws", `
-"--ws.addr=0.0.0.0", `
-"--ws.port=7878", `
-"--pandora-rpc-endpoint=ws://127.0.0.1:8546", `
-"--verbosity=trace" -NoNewWindow -RedirectStandardOutput "orchestrator_$runDate.out" -RedirectStandardError "orchestrator_$runDate..err"
-#    | Out-File -FilePath $logsdir/orchestrator/orchestrator_"$RUN_DATE".log
+    Write-Output $runDate | Out-File -FilePath "$logsdir\orchestrator\current.tmp"
 
+    $arguments = @(
+        "--datadir=$datadir\orchestrator"
+        "--vanguard-grpc-endpoint=127.0.0.1:4000"
+        "--http"
+        "--http.addr=0.0.0.0"
+        "--http.port=7877"
+        "--ws"
+        "--ws.addr=0.0.0.0"
+        "--ws.port=7878"
+        "--pandora-rpc-endpoint=ws://127.0.0.1:8546"
+        "--verbosity=trace"
+    )
+
+    Write-Output $arguments
+
+    Start-Process -FilePath "lukso-orchestrator" `
+    -ArgumentList $arguments `
+    -NoNewWindow -RedirectStandardOutput "orchestrator_$runDate.out" -RedirectStandardError "orchestrator_$runDate..err"
 
 
 }
@@ -89,9 +150,27 @@ function _start($client) {
     }
 }
 
+##Flags action
+if ($orchestrator) {
+    bind_binary orchestrator $orchestrator
+}
+
+if ($pandora) {
+    bind_binary pandora $pandora
+}
+
+if ($vanguard) {
+    bind_binary vanguard $vanguard
+}
+
+if ($validator) {
+    bind_binary validator $validator
+}
+
 switch ($command) {
 start {
     _start $argument
+    $KeepShell = $true
 }
 
 stop {
@@ -111,12 +190,20 @@ logs {
 }
 
 bind-binaries {
-
+    Write-Output binding
 }
 
 Default {
     Write-Output "Unknown command"
     exit
 }
+}
+
+if ($KeepShell) {
+    Write-Output "LUKSO clients are working do not close this shell"
+}
+
+while ($KeepShell) {
+    Read-Host
 }
 
