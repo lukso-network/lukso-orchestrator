a044f088d6cb937bcf6721db6ca3c7570a1ad8a8
Atif Anowar <radi.cse072@gmail.com>
2021-10-30 20:22:30 +0200

    Add unit test for re-org and add simple l15 resume mechanism

---
+++ orchestrator/db/kv/reorg_db_test.go
@@ -0,0 +1,64 @@
+package kv
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/lukso-network/lukso-orchestrator/shared/testutil"
+	"github.com/lukso-network/lukso-orchestrator/shared/testutil/require"
+	"github.com/lukso-network/lukso-orchestrator/shared/types"
+	"testing"
+)
+
+func TestStore_ReorgDB(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	db := setupReorgDB(t, ctx)
+	defer db.ClearDB()
+	reorgEpochInfo := &types.MinimalEpochConsensusInfoV2{
+		Epoch: 3,
+		ReorgInfo: &types.Reorg{
+			VanParentHash: []byte{uint8(50)},
+			PanParentHash: []byte{uint8(100)},
+		},
+	}
+	require.NoError(t, db.RevertConsensusInfo(reorgEpochInfo))
+	require.Equal(t, uint64(2), db.LatestSavedEpoch())
+	require.Equal(t, uint64(50), db.LatestSavedVerifiedSlot())
+	require.Equal(t, common.BytesToHash([]byte{uint8(100)}), db.LatestVerifiedHeaderHash())
+	expectedSlotInfo := (*types.SlotInfo)(nil)
+	for i := 51; i <= 100; i++ {
+		actualSlotInfo, err := db.VerifiedSlotInfo(uint64(i))
+		require.NoError(t, err)
+		require.DeepEqual(t, expectedSlotInfo, actualSlotInfo)
+	}
+
+	for i := 0; i <= 50; i++ {
+		expectedSlotInfo := new(types.SlotInfo)
+		expectedSlotInfo.VanguardBlockHash = common.BytesToHash([]byte{uint8(i)})
+		expectedSlotInfo.PandoraHeaderHash = common.BytesToHash([]byte{uint8(i + 50)})
+
+		actualSlotInfo, err := db.VerifiedSlotInfo(uint64(i))
+		require.NoError(t, err)
+		require.DeepEqual(t, expectedSlotInfo, actualSlotInfo)
+	}
+}
+
+func setupReorgDB(t *testing.T, ctx context.Context) *Store {
+	db := setupDB(t, true)
+	for i := 0; i < 5; i++ {
+		consensusInfo := testutil.NewMinimalConsensusInfo(uint64(i))
+		epochInfoV2 := consensusInfo.ConvertToEpochInfo()
+		require.NoError(t, db.SaveConsensusInfo(ctx, epochInfoV2))
+	}
+	for i := 0; i <= 100; i++ {
+		slotInfo := new(types.SlotInfo)
+		slotInfo.VanguardBlockHash = common.BytesToHash([]byte{uint8(i)})
+		slotInfo.PandoraHeaderHash = common.BytesToHash([]byte{uint8(i + 50)})
+		require.NoError(t, db.SaveVerifiedSlotInfo(uint64(i), slotInfo))
+	}
+
+	require.NoError(t, db.SaveLatestEpoch(ctx))
+	require.NoError(t, db.SaveLatestVerifiedSlot(ctx))
+	require.NoError(t, db.SaveLatestVerifiedHeaderHash())
+	return db
+}
